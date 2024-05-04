5c08ff3e88333338bbf3ea9920f731277513dafd
Vera Yu <qingyu9243@gmail.com>
2024-05-03 21:12:47 -0700

    Update union_find.py

--- By Topic/Algorithm/union_find.py
+++ By Topic/Algorithm/union_find.py
@@ -1,4 +1,24 @@
+"""
+0 -- 1 -- 2
+     |
+3 -- 4 -- 5
+
+root: [0, 1, 1, 3, 3, 5]
+index: 0, 1, 2, 3, 4, 5
+"""
+
+class UnionFind:
+    def __init__(self) -> None:
+        pass
+
+    def union(self, x, y):
+        pass
+    def find(self, x):
+        pass
+
+
 # 200	Number of Islands 59.5%	Medium	
+
 # 128	Longest Consecutive Sequence	47.3%	Medium	
 # 827	Making A Large Island	47.1%	Hard	
 # 399	Evaluate Division	61.7%	Medium	
