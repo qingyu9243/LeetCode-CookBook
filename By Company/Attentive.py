c1ccf35fd7d555ff6169c42933b1423e1ef64b39
Vera Yu <qingyu9243@gmail.com>
2024-06-27 20:43:49 -0700

    Update Attentive.py

--- By Company/Attentive.py
+++ By Company/Attentive.py
@@ -2,6 +2,11 @@
 """
 Design a system that ingests events from an API for shopping cart status (add to cart, purchased, remove from cart, etc) and serves up abandoned cart messages to users after 30 minutes.
 
+1. Find top k most frequent words 
+2. Design LRU cache 
+3. Design a photo upload service 
+4. Evaluate expression - https://www.geeksforgeeks.org/expression-evaluation/
 
+How would you build Twitter for locations?
 """
 
