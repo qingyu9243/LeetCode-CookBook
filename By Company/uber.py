3de175ef04c20c3abb885624097060498dc1cadd
Vera Yu <qingyu9243@gmail.com>
2024-07-17 05:34:40 +0800

    Update uber.py

--- By Company/uber.py
+++ By Company/uber.py
@@ -14,9 +14,85 @@ class AllOne:
 
     def getMinKey(self,):
         pass
-# 2508	Add Edges to Make Degrees of All Nodes Even	31.6%	Hard	
+
+# 2508	Add Edges to Make Degrees of All Nodes Even	31.6%	Hard
+from typing import List
+def isPossible(self, n: int, edges: List[List[int]]) -> bool:
+    # graph dict to store in degree nodes for each node as a set
+    graph = defaultdict(set)
+    for n1, n2 in edges:
+        graph[n1].add(n2)
+        graph[n2].add(n1)
+    #print(graph)
+
+    # find all nodes with odd degrees
+    odd_degree_nodes = []
+    for i in range(1, n+1):
+        if len(graph[i]) % 2 == 1:
+            odd_degree_nodes.append(i)
+    #print(odd_degree_nodes)
+
+    # if no odd degree nodes, return true
+    if len(odd_degree_nodes) == 0:
+        return True
+    
+    # if exactly 2 odd degree nodes
+    if len(odd_degree_nodes) == 2:
+        n1, n2 = odd_degree_nodes
+        # connect n1 and n2
+        if n1 not in graph[n2]:
+            return True
+        # connect n1, n2 with another node
+        for node in range(1, n+1):
+            if node != n1 and node != n2 and node not in graph[n1] and node not in graph[n2]:
+                return True
+    
+    # if extactly 4 odd degree nodes
+    if len(odd_degree_nodes) == 4:
+        a, b, c, d = odd_degree_nodes
+        possible_pairs = [
+            (a, b, c, d),
+            (a, c, b, d),
+            (a, d, b, c)
+        ]
+        for u1, v1, u2, v2 in possible_pairs:
+            if v1 not in graph[u1] and v2 not in graph[u2]:
+                return True
+            if v2 not in graph[u1] and v1 not in graph[u2]:
+                return True
+    return False
+
 # 564	Find the Closest Palindrome	22.5%	Hard
-	
+def nearestPalindromic(self, n: str) -> str:
+    # think about the example of "12345"
+    # case 1: same first half, and palindrome, 123321
+    # case 2: first half + 1, and palindrome, 12221
+    # case 3: first half - 1, and palindrome, 12421
+    # case 4: less digits, and palindrome, 9999
+    # case 5: more digits, and palindrome, 100001
+    def helper(n, additive):
+        l = len(n)
+        if l == 0:
+            return 0
+        first_half = str(int(n[:l // 2 + l % 2]) + additive)
+        return int(first_half + first_half[(-1 - l%2)::-1])
+    res = None
+    pals = [
+        helper(n, 0),
+        helper(n, 1),
+        helper(n, -1),
+        helper("9"*(len(n) - 1), 0),
+        helper("1" + "0" * (len(n)), 0)
+    ]
+    print(pals)
+    for i in pals:
+        if str(i) == n:
+            pass
+        else:
+            if res is None or abs(res - int(n)) > abs(i - int(n)) or (abs(res - int(n)) == abs(i - int(n)) and i < int(n)):
+                res = i
+    return str(res)	
+
 # 815	Bus Routes	47.8%	Hard
 from collections import deque
 from collections import defaultdict
