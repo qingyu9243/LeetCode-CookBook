5d6f973d57b2d20bd3179a1028661ea1cc429dac
Vera Yu <qingyu9243@gmail.com>
2025-04-25 11:04:25 -0700

    Update samsung.py

--- By Company/samsung.py
+++ By Company/samsung.py
@@ -55,6 +55,22 @@ def possibleBipartition(n, dislikes):
     
     return True
 
+def numSubarrayProductLessThanK(nums, k):
+    if k <= 1:
+        return 0
+    
+    product = 1
+    count = 0
+    left = 0
+    for right in range(len(nums)):
+        product *= nums[right]
+        while product >= k:
+            product /= nums[left]
+            left += 1
+        count += right - left + 1
+
+    return count
+
 # 785. Is Graph Bipartite? - color dfs
 def isGraphBipartite(graph):
     n = len(graph)
