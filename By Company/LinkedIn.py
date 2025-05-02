fd9e3f022df9534ce225bb3b09e11d251d03ed6b
Vera Yu <qingyu9243@gmail.com>
2025-05-02 11:59:44 -0700

    Update LinkedIn.py

---
+++ By Company/LinkedIn.py
@@ -0,0 +1,107 @@
+
+# 432. All O's one data structure
+
+
+class AllOne:
+    def __init__(self) -> None:
+        pass
+    
+    def inc(self, key):
+        pass
+
+    def dec(self, key):
+        pass
+
+    def getMaxKey(self):
+        pass
+
+    def getMinKey(self):
+        pass
+
+# 364.
+
+# 716. Max Stack
+
+# 20. Valid Parentheses
+
+# 50. Pow(x, n)
+
+# 244. Shortest Word Distance II
+
+# 277. Find the celebrity
+
+# 53. Maximum Subarray
+
+# 127. Word Ladder
+
+# 366. Find leaves of Binary Tree
+
+# 380. Insert Delete GetRandom
+
+# 636. Exclusive TIme of 
+
+# 200. Number of Islands
+
+# 3480. Maximize Subarrays after
+
+# 605. Can place flowers
+
+# 33. Search in Rotated Sorted Array
+
+# 17. letter of comb
+
+# 156. Binary Tree upside down
+
+# 215. Kth largest element in 
+
+# 256. Paint House
+
+# 272. Closet binary search Tree
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
+
+#
